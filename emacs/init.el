391f0c54abec54aa5d6e56c35948ecb135a8e1e1
Alex Seubert <alexseubert@gmail.com>
2015-07-24 10:52:32 -0500

    Frontend dev env emacs stuff

--- emacs/init.el
+++ emacs/init.el
@@ -42,15 +42,31 @@
 (setq-default hs-minor-mode t)
 (setq default-tab-width 2)
 
+(defun jshint-init ()
+  (setq-local jshint-configuration-path
+              (expand-file-name ".jshintrc"
+                                (locate-dominating-file
+                                 default-directory ".jshintrc")))
+  (flymake-jshint-load))
+
 ;; jslint me
 (require 'flymake-jshint)
-(add-hook 'js-mode-hook (lambda ()
-                          (setq-local jshint-configuration-path
-                                      (expand-file-name ".jshintrc"
-                                                        (locate-dominating-file
-                                                         default-directory ".jshintrc")))
-                          (flymake-jshint-load)))
+(add-hook 'js-mode-hook 'jshint-init)
 
+;; React
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
+    ad-do-it))
+(setq web-mode-code-indent-offset 2)
+(setq web-mode-markup-indent-offset 2)
+(add-hook 'web-mode-hook 'jshint-init)
+
+(setq python-shell-interpreter "python3")
+
+;; Templates for Pyramid :/
 (add-to-list 'auto-mode-alist
              '("\\.pt\\'" . (lambda ()
                               (html-mode))))
@@ -72,7 +88,34 @@
 (setq js-indent-level 2)
 (setq css-indent-offset 2)
 
+(setenv "PATH" (concat "~/.cabal/bin:" (getenv "PATH")))
+(setenv "PATH" (concat "/opt/ghc/7.8.4/bin:" (getenv "PATH")))
+(add-to-list 'exec-path "~/.cabal/bin")
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+(custom-set-variables '(haskell-tags-on-save t))
+(custom-set-variables
+  '(haskell-process-suggest-remove-import-lines t)
+  '(haskell-process-auto-import-loaded-modules t)
+  '(haskell-process-log t))
+(eval-after-load 'haskell-mode '(progn
+  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+  (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+  (define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
+  (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
+  (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
+  (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)
+  (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)))
+(eval-after-load 'haskell-cabal '(progn
+  (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+  (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+  (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+  (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)))
+(custom-set-variables '(haskell-process-type 'cabal-repl))
+
+(autoload 'ghc-init "ghc" nil t)
+(autoload 'ghc-debug "ghc" nil t)
+(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
 
 ;; show time
 (setq display-time-day-and-date t
