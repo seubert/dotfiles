059d163ec6ad1f0b84e13c8a3671fe8aaad74c7a
Alex Seubert <alexseubert@gmail.com>
2025-05-22 19:48:03 -0500

    Add helix, wezterm, zellij

---
+++ private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'Everforest Dark Soft (Gogh)'
+
+config.use_fancy_tab_bar = false
+config.show_tabs_in_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+
+return config
